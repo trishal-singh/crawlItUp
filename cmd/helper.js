4da279e24b674ad4bf2ac437799d3027446062d4
trishal-singh <trishalsinghhcwheel@gmail.com>
2024-01-12 20:05:27 +0530

    added normalizeUrl function with tests

--- cmd/helper.js
+++ cmd/helper.js
@@ -1,6 +1,10 @@
 function normalizeUrl(path)
 {
-  return path
+  const url=new URL(path)
+  let newurl= url.hostname + url.pathname
+  if(newurl.length>0 && newurl.slice(-1)==='/')
+   newurl=newurl.slice(0,-1)
+  return newurl
 }
 module.exports = {
     normalizeUrl
--- cmd/helper.test.js
+++ cmd/helper.test.js
@@ -1,9 +1,19 @@
 const { test, expect } = require('@jest/globals')
 const { normalizeUrl } = require('./helper.js')
 
-test("normalizeUrl empty",()=>{
-    expect(normalizeUrl("")).toBe("")
+
+test("normalizeUrl url 1",()=>{
+    expect(normalizeUrl("https://blog.boot.dev/path/")).toBe("blog.boot.dev/path")
 })
-test("normalizeUrl empty 2",()=>{
-    expect(normalizeUrl(" ")).toBe(" ")
+
+test("normalizeUrl url 2",()=>{
+    expect(normalizeUrl("https://blog.boot.dev/path")).toBe("blog.boot.dev/path")
+})
+
+test("normalizeUrl url 2",()=>{
+    expect(normalizeUrl("http://blog.boot.dev/path/")).toBe("blog.boot.dev/path")
+})
+
+test("normalizeUrl url 4",()=>{
+    expect(normalizeUrl("http://blog.boot.dev/path")).toBe("blog.boot.dev/path")
 })
