228d3586f34d425f418ffd9aaed3fdc298e848d8
trishal-singh <trishalsinghhcwheel@gmail.com>
2024-01-14 23:31:38 +0530

    read cmd arguments

--- cmd/main.js
+++ cmd/main.js
@@ -1,1 +1,14 @@
-console.log("Running")
+function main(){
+    if (process.argv.length < 3){
+      console.log('no website provided')
+    }
+    if (process.argv.length > 3){
+      console.log('too many arguments provided')
+    }
+  
+    const baseURL = process.argv[2]
+  
+    console.log(`processing: ${baseURL}...`)
+  }
+  
+  main()
